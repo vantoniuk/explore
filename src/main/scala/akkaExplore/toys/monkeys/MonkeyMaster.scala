5b8eb920e659bdfd178a596ec5f9d161d3139df9
vantoniuk <vantoniuk@gmail.com>
2014-01-25 21:13:37 -0800

    slight improving in logging

--- src/main/scala/akkaExplore/toys/monkeys/MonkeyMaster.scala
+++ src/main/scala/akkaExplore/toys/monkeys/MonkeyMaster.scala
@@ -8,8 +8,10 @@ import akkaExplore.toys.monkeys.MonkeyMessage.TypingGoal
 
 class MonkeyMaster(config: MonkeyConfig) extends Actor with ActorLogging  {
   lazy val monkeys: Seq[ActorRef] = prepareMonkeys(config.numberOfTypists, config.symbolsToType)
+
   var messageQueue: Seq[(String, ActorRef)] = Seq.empty
-  var results: List[String] = Nil
+  var results: Vector[String] = Vector.empty
+  val tasksAtOneTime: Int = 1000 //Runtime.getRuntime.availableProcessors() * 10
 
   log.info(s"Started Monkey Typist system for ${config.numberOfTypists} monkeys")
 
@@ -23,11 +25,11 @@ class MonkeyMaster(config: MonkeyConfig) extends Actor with ActorLogging  {
       else prepare(left - 1, context.actorOf(Props(new MonkeyTypist(symbolsToType))) :: monkeys)
     }
 
-    prepare(monkeysToType, Nil)
+    prepare(tasksAtOneTime, Nil)
   }
 
   def busy: Receive = {
-    case TypingResult(result) =>  receiveResult(result)
+    case TypingResult(result) => receiveResult(result)
     case _ =>
       log.info(s"current capacity ${messageQueue.size}")
       sender ! Busy
@@ -44,33 +46,49 @@ class MonkeyMaster(config: MonkeyConfig) extends Actor with ActorLogging  {
         //if the queue is too large, become busy
         if(messageQueue.size >= config.capacity) context.become(busy)
         log.info(s"sending task to ${config.numberOfTypists} monkeys")
-        if(results.isEmpty) monkeys foreach (_ ! DoTyping(config.typingMachine.alphabet))
+        if(results.isEmpty) putMonkeysToWork(monkeys)
       }
-    case TypingResult(result) =>  receiveResult(result)
+    case TypingResult(result) => receiveResult(result)
   }
 
   def receiveResult(result: String) {
-    results = result :: results
+    results = results :+ result
     val resultsSoFar = results.length
     if(resultsSoFar == config.numberOfTypists) {
-      log.info("got all the results")
+      log.info("got all the results, searching for best....")
       val (goal, recipient) = messageQueue.head
       recipient ! TypingResult(findBestResult(results, goal))
       messageQueue = messageQueue.tail
-      results = Nil
+      results = Vector.empty
     } else {
       //log status
-      if(resultsSoFar % (config.numberOfTypists / 10) == 0)
+      if(resultsSoFar % tasksAtOneTime == 0) putMonkeysToWork(monkeys)
+      if(resultsSoFar % (config.numberOfTypists / 100) == 0)
         log.info(s"collected $resultsSoFar results out of ${config.numberOfTypists}")
     }
   }
 
-  private def findBestResult(results: List[String], goal: String): String = {
+  private def findBestResult(results: Vector[String], goal: String): String = {
     val splittedGoal = goal.split(config.typingMachine.space).toList.distinct
 
-    (results map { r =>
+    val resultsAbsolute = (results map { r =>
       splittedGoal intersect r.split(config.typingMachine.space).toList
     } sortBy (- _.length)).head mkString config.typingMachine.space.toString
+
+    if(resultsAbsolute.nonEmpty) {
+      resultsAbsolute
+    }
+    else {
+      log.info("no absolute match")
+      val splittedGoalSet = splittedGoal.toSet
+      (results map { r => r.split(config.typingMachine.space).toList.filter( splittedGoalSet(_) )
+      } sortBy (- _.length)).head mkString config.typingMachine.space.toString
+
+    }
+  }
+
+  def putMonkeysToWork(availableMonkeys: Seq[ActorRef]) {
+    availableMonkeys foreach (_ ! DoTyping(config.typingMachine.alphabet))
   }
 
 }
